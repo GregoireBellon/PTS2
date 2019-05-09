ac5474de216cd5b30a1b6f70e7edc978d7c5792e
Grégoire Bellon <Gregoire.bellon@outlook.fr>
2019-05-09 17:12:32 +0200

    Création case

--- Code/src/Case.java
+++ Code/src/Case.java
@@ -1,4 +1,18 @@
 
 public class Case {
 
+private String type;	
+	
+	public Case(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type=type;
+	}
+	
 }
