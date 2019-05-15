fdc9e40184f3ee2297150a214c70b6f2c955d337
gameflave <gameflave@gmail.com>
2019-05-15 11:39:16 +0200

    Deplacement du plateau de la classe Defi à la Classe Jeux

--- Code/src/jeux.java
+++ Code/src/jeux.java
@@ -1,9 +1,19 @@
 
-public class jeux {
-
+public class Jeux {
 	
-	public boolean placerPiece(Piece piece) {
-		return false;
-		
+	private static Case[][] plateau = new Case[4][4];
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
+
+	public static Case[][] getPlateau() {
+		return plateau;
+	}
+
+	public void setPlateau(Case[][] plateau) {
+		this.plateau = plateau;
+	}
+
 }
